a0d1214f7ca9aa4090e23f6746fcdfb98c3cb7f5
Pscly <Pscly@outlook.com>
2021-11-22 13:45:39 +0800

    更新细节

--- d1/a1猜数字.py
+++ d1/a1猜数字.py
@@ -1,13 +1,13 @@
 import time
 import datetime
 
-    class Solution:
-        def game(self, guess: list[int], answer: list[int]) -> int:
-            count = 0
-            for i in range(3):
-                if guess[i] == answer[i]:
-                    count += 1
-            return count
+class Solution:
+    def game(self, guess: list[int], answer: list[int]) -> int:
+        count = 0
+        for i in range(3):
+            if guess[i] == answer[i]:
+                count += 1
+        return count
     
 start_time = datetime.datetime.now()
 x = Solution().game([1,2,3],[1,2,3])
